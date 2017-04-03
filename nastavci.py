4111038d1daa01c7c53c48a4534a70ab104c8654
Ivan Krišto <ivan.kristo@gmail.com>
2017-04-03 22:22:14 +0200

    maknuo py fileove

--- nastavci.py
+++
@@ -1,7 +0,0 @@
-import re
-import sys
-
-ls = sys.stdin.readlines()
-for l in ls:
-    print re.sub(r"\-([a-z\\\"]+)", r"\\nastavak{\1}", l),
-
--- primjer.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-ls = sys.stdin.read()
-for l in ls:
-    lr = l.split("->")
-    left = lr[0].strip()
-    right = lr[1].strip()
-    print r"\primjer{%s \\rightarrow %s}"
